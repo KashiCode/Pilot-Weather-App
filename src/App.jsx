d5b9b615e53b4654b26414489814f40ab9b678af
Maks Ostrynski <ostrynskimaks@gmail.com>
2025-04-02 01:27:36 +0100

    Transfered Aiport list to App,jsx
    - allows airport list to be used by top and bottom dashboards.
    - allows quick report section to use airport list.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import TopDashboard from "./Pages/TopDashboard";
 import BottomDashboard from "./Pages/BottomDashboard";
 
+const airports = [
+  { code: "EGLL", city: "London", name: "London Heathrow" },
+  { code: "LFPG", city: "Paris", name: "Paris Charles de Gaulle" },
+  { code: "EDDF", city: "Frankfurt", name: "Frankfurt Airport" },
+  { code: "OMDB", city: "Dubai", name: "Dubai International" },
+  { code: "RJTT", city: "Tokyo", name: "Tokyo Haneda" },
+];
+
 const App = () => {
+  const [selectedAirport, setSelectedAirport] = useState(airports[0]);
+
   return (
     <div className="bg-[#161f2c] text-white relative bg-[radial-gradient(ellipse_50.00%_50.00%_at_50.00%_50.00%,_#253244_0%,_#141D2A_100%)] overflow-hidden font-normal font-['Inter']">
-      <TopDashboard/>
-      <hr/>
-      <BottomDashboard/>
+      <TopDashboard
+        selectedAirport={selectedAirport}
+        setSelectedAirport={setSelectedAirport}
+        airports={airports}
+      />
+      <hr />
+      <BottomDashboard selectedAirport={selectedAirport} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
